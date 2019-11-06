f5d88afff71461b97e1f96c14b04d7efefc17a00
KimHanjung <rlagkswnd23@gmail.com>
2019-11-06 20:50:25 +0900

    v1.0

--- client.py
+++ client.py
@@ -12,13 +12,13 @@ class ChatClient():
     def main(self, parent, args):
         if(args.user == -1):
             print("invalid user id, assign --user [user_id]. (user_id > 0)")
-        
+
         try:
             clientSock = socket(AF_INET, SOCK_STREAM) # TCP
             clientSock.connect(('127.0.0.1', args.port)) # localhost
             clientSock.send(('user_id:' + str(args.user)).encode('utf-8'))
             # clientSock.recv(args.max_data_recv).decode('utf-8')
-            
+
             _thread.start_new_thread(self.send_thread, (clientSock, args))
             _thread.start_new_thread(self.recv_thread, (parent, clientSock, args))
 
@@ -36,8 +36,8 @@ class ChatClient():
                 break
             else:
                 _clientSock.send((input_str.split(' ')[0] + "#" + input_str.split(' ', 1)[1]).encode('utf-8'))
-        
-        
+
+
 
     def recv_thread(self, parent, _clientSock, args):
         while True:
@@ -45,14 +45,18 @@ class ChatClient():
             if(data.find('[error]') != -1):
                 print(data)
                 break
+            elif(data.find('[exit]') != -1):
+                break
+            elif(len(data) == 0):
+                break
             else:
-                print(data, end=' ') 
+                print(data, end=' ')
                 if(parent is not None):
                     parent.lb2.setText(parent.lb2.text() + '\n\n' + data)
 
         print('connection closed')
         _clientSock.close()
-            
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -60,4 +64,4 @@ if __name__ == "__main__":
     parser.add_argument('--user', type=int, default=-1)
     parser.add_argument('--port', type=int, default=8081) # server port
     args = parser.parse_args()
-    chatClient = ChatClient(args)
+    chatClient = ChatClient(None, args)
