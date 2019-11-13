c16d1016169a1522116066dca2b33d7a2fa06930
bluehope <ms7045436@likelion.org>
2019-11-14 01:18:44 +0900

    server/client stability

--- client.py
+++ client.py
@@ -5,72 +5,107 @@ from socket import *
 import argparse
 
 class ChatClient():
+    closed = False
+
     def __init__(self, parent, args):
         super().__init__()
         self.main(parent, args)
 
     def main(self, parent, args):
-        if(args.user == -1):
-            print("invalid user id, assign --user [user_id]. (user_id > 0)")
+        if(args.username == ''):
+            print("invalid username, assign --username [username]. (user_id cannot be blanked)")
 
         try:
+            if args.cli == 1:
+                destinationAddr = '127.0.0.1'
+            else:
+                destinationAddr = '13.125.249.160'
+
             clientSock = socket(AF_INET, SOCK_STREAM) # TCP
-            clientSock.connect(('13.125.249.160', args.port)) # localhost
-            clientSock.send(('user_id:' + str(args.user) + ':' + args.username).encode('utf-8'))
+            clientSock.connect((destinationAddr, args.port)) # localhost
+            clientSock.send(('###STARTCONNECT###' + args.username).encode('utf-8'))
             # clientSock.recv(args.max_data_recv).decode('utf-8')
 
             _thread.start_new_thread(self.send_thread, (parent, clientSock, args))
             _thread.start_new_thread(self.recv_thread, (parent, clientSock, args))
 
-            while True:
-                time.sleep(1)
-                pass
+            try:
+                while True:
+                    time.sleep(1)
+                    if self.closed:
+                        break
+                    pass
+            except KeyboardInterrupt:
+                clientSock.send('###EXIT###'.encode('utf-8'))
+                self.printmsg(parent, '[SYSTEM] connection closed')
+                clientSock.close()
+                sys.exit()
+                
         except OSError as e:
             print(e)
 
 
     def send_thread(self, parent, _clientSock, args):
         while True:
-            if not parent.messageQueue.qsize():
-                input_str = parent.messageQueue.get()
-                if input_str == 'exit':
-                    _clientSock.send('exit'.encode('utf-8'))
-                    break
-                else:
-                    _clientSock.send((input_str.split(' ')[0] + "#" + input_str.split(' ', 1)[1]).encode('utf-8'))
+            if(parent):
+                if not parent.messageQueue.qsize():
+                    input_str = parent.messageQueue.get()
+                    if input_str == 'exit':
+                        _clientSock.send('###EXIT###'.encode('utf-8'))
+                        break
+                    elif input_str.find("###") != -1:
+                        self.printmsg(parent, "you can't use ### in your input")
+                    else:
+                        _clientSock.send(("###DATA###" + input_str.split(' ')[0] + "#" + input_str.split(' ', 1)[1]).encode('utf-8'))
             else:
-                input_str = input(">>> ")
+                input_str = input("")
                 if input_str == 'exit':
-                    _clientSock.send('exit'.encode('utf-8'))
+                    _clientSock.send('###EXIT###'.encode('utf-8'))
                     break
+                elif input_str.find("###") != -1:
+                    print("you can't use ### in your input")
                 else:
-                    _clientSock.send((input_str.split(' ')[0] + "#" + input_str.split(' ', 1)[1]).encode('utf-8'))
-
+                    _clientSock.send(('###DATA###' + input_str.split(' ')[0] + "#" + input_str.split(' ', 1)[1]).encode('utf-8'))
+        
+        _clientSock.close()
+        self.closed = True
+        sys.exit()
 
     def recv_thread(self, parent, _clientSock, args):
         while True:
-            data = _clientSock.recv(args.max_data_recv).decode('utf-8')
-            if(data.find('[error]') != -1):
-                print(data)
+            try:
+                data = _clientSock.recv(args.max_data_recv).decode('utf-8')
+            except OSError as e:
                 break
-            elif(data.find('[exit]') != -1):
+            if(data.find('###ERROR###') != -1):
+                self.printmsg(parent, '[SYSTEM] ' + (data.split('###ERROR###')[1]))
                 break
+            elif(data.find('###WARNING###') != -1):
+                self.printmsg(parent, '[SYSTEM] ' + (data.split('###WARNING###')[1]))
             elif(len(data) == 0):
                 break
             else:
-                print(data, end=' ')
-                if(parent):
-                    parent.textBrowser.append(data)
+                if data.find('###CONNECTSUCCESS###') != -1:
+                    self.printmsg(parent, ('[SYSTEM] ' + args.username + ' is successfully connected'))
+                else:
+                    self.printmsg(parent, data)
 
-        print('connection closed')
+        self.printmsg(parent, '[SYSTEM] connection closed')
         _clientSock.close()
+        self.closed = True
+        sys.exit()
+    
+    def printmsg(self, parent, msg):
+        print(msg)
+        if(parent):
+            parent.textBrowser.append(msg)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument('--cli', type=int, default=0)
     parser.add_argument('--max_data_recv', type=int, default=4096) # byte
-    parser.add_argument('--user', type=int, default=-1)
-    parser.add_argument('--username', type=str, default='xxx')
+    parser.add_argument('--username', type=str, default='')
     parser.add_argument('--port', type=int, default=8081) # server port
     args = parser.parse_args()
     chatClient = ChatClient(None, args)
--- server.py
+++ server.py
@@ -4,16 +4,13 @@ from socket import *
 import argparse
 
 class ChatServer():
-    connection_pool = []
+    connection_pool = {}
     _render_msg = "init"
 
     def __init__(self, parent, args):
         self.main(parent, args)
 
     def main(self, parent, args):
-        for i in range(args.backlog):
-            self.connection_pool.append(None)
-
         try:
             serverSock = socket(AF_INET, SOCK_STREAM) # TCP
             serverSock.bind(('', args.port)) # localhost
@@ -24,56 +21,47 @@ class ChatServer():
             if serverSock:
                 serverSock.close()
             print(e)
-            sys.exit(1)
+            sys.exit()
 
         print('..listening..')
 
         while 1:
             connectionSock, client_addr = serverSock.accept()
             if(connectionSock):
-                print("accepted")
                 _thread.start_new_thread(self.connection_thread, (parent, connectionSock, client_addr, args))
 
         serverSock.close()
 
     def connection_thread(self, parent, _connectionSock, _client_addr, args):
-        user = None
         username = ""
-        while 1:
+        while True:
             request = _connectionSock.recv(args.max_data_recv).decode('utf-8')
-            print(request)
-            if(request == 'exit'):
-                _connectionSock.send(('[exit]').encode('utf-8'))
+
+            if(request != ''):
+                print(username, request)
+            if(request == '###EXIT###'): # client exit handling
                 _connectionSock.close()
+                self.connection_pool.pop(username)
                 print('connection closed')
                 break
-            if(request == ''):
-                _connectionSock.send(('[exit]').encode('utf-8'))
-                _connectionSock.close()
-                print('connection closed')
-                break
-            if(request.split(':')[0] == 'user_id'): # first meet
-                if self.connection_pool[int(request.split(':')[1])] == None:
-                    user = int(request.split(':')[1])
-                    username = request.split(':')[2]
-                    self.connection_pool[user] = _connectionSock
-                    _connectionSock.send(("user[" + username + "] is connected").encode('utf-8'))
-                    # _connectionSock.send(('connected user ' + request.split(':')[1]).encode('utf-8'))
+            if(request.find('###STARTCONNECT###') != -1): # first meet
+                request = request.split('###STARTCONNECT###')[1]
+                if request not in self.connection_pool:
+                    username = request
+                    self.connection_pool[username] = _connectionSock
+                    _connectionSock.send(('###CONNECTSUCCESS###').encode('utf-8'))
                 else:
-                    _connectionSock.send('[error] invalid access detected'.encode('utf-8'))
-                    _connectionSock.close()
-                    self.connection_pool[int(request.split(':')[1])] = None
+                    _connectionSock.send('###ERROR###the username already used by someone'.encode('utf-8'))
                     print('connection closed')
                     break
-            else:
-                if(self.connection_pool[int(request.split('#', 1)[0])] == None):
-                    _connectionSock.send(('[error] user' + (request.split('#', 1)[0]) + ' not connected').encode('utf-8'))
-                    _connectionSock.close()
-                    self.connection_pool[user] = None
-                    print('connection closed')
-                    break
+            elif(request.find('###DATA###') != -1): # else meet
+                request = request.split('###DATA###')[1]
+                if(request.split('#', 1)[0] not in self.connection_pool):
+                    _connectionSock.send(('###WARNING###' + (request.split('#', 1)[0]) + ' not connected').encode('utf-8'))
+                elif request.split('#', 1)[0] == username:
+                    _connectionSock.send(('###WARNING###' + (request.split('#', 1)[0]) + ' is you').encode('utf-8'))
                 else:
-                    self.connection_pool[int(request.split('#', 1)[0])].send(("\n[user" + str(user) + "]: " + request.split('#')[1]).encode('utf-8'))
+                    self.connection_pool[request.split('#', 1)[0]].send(("[" + username + "]: " + request.split('#')[1]).encode('utf-8'))
 
     def render_msg(self):
         return self._render_msg
